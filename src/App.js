6e1cbcd7fa40bdb235c1d14ac5abd60743e1a18e
Gkhachapuridze <giorgi.khatchapuridze.1@gmail.com>
2020-05-09 19:38:22 +0400

    Retro Mini Clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [date, setDate] = useState(new Date());
+
+  useEffect(()=> {
+    setInterval(() => setDate(new Date()),1000)
+  },[date]);
+
+  let num1 = "";
+  let num2 = "";
+  let num3 = "";
+  let num4 = "";
+  if (date.getHours() < 10) {
+    num1 =  "0";
+    num2 = date.getHours();
+  } else {
+    num1 = date.getHours().toString()[0];
+    num2 = date.getHours().toString()[1];
+  }
+  if (date.getMinutes() < 10) {
+    num3 = "0";
+    num4 = date.getMinutes();
+  } else {
+    num3 = date.getMinutes().toString()[0];
+    num4 = date.getMinutes().toString()[1];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="clock">
+          <div className="mainbody">
+            <div className="backbody">
+              <div className="colont"></div>
+              <div className="colonb"></div>
+              <div className="digit1">
+                <div className="num1">{num1}</div>
+                <div className="brace"></div>
+              </div>
+              <div className="digit2">
+                <div className="num2">{num2}</div>
+                <div className="brace"></div>
+              </div>
+              <div className="digit3">
+                <div className="num3">{num3}</div>
+                <div className="brace"></div>
+              </div>
+              <div className="digit4">
+                <div className="num4">{num4}</div>
+                <div className="brace"></div>
+              </div>
+            </div>
+          </div>
+          <div className="footl">
+            <div className="inner"></div>
+          </div>
+          <div className="footr">
+            <div className="inner"></div>
+          </div>
+          <div className="shadow"></div>
+        </div>
+
       </header>
     </div>
   );
